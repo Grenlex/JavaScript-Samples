41d1fda23642d7e26eefede3f62d80ffb0451b8f
Alexey Shmelev <60858323+Grenlex@users.noreply.github.com>
2020-04-09 16:14:04 +0200

    Create Device Definer.js

---
+++ Device Definer.js
@@ -0,0 +1,12 @@
+<script language="JavaScript">
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  
+    // code for smartphones
+    
+  } else {
+  
+    // code for desktops
+    
+  }
+
+</script>
